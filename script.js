662da92f6bf372ebd47d3fb759fae96e96c04bef
Casper Werner <cappe1996@hotmail.com>
2022-11-08 16:56:36 +0100

    halft klar med den, behöver få det att lägga sig uppifrån ner

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let date = document.querySelector("#date");
 let textContent = document.querySelector("#textContent");
 let submit = document.querySelector(".submit");
 let divinput = document.querySelector(".input");
-let arrayOfBlog = [];
+
 let outputs = document.querySelector(".output");
 let container = document.querySelector(".container");
 
@@ -32,6 +32,13 @@ submit.addEventListener("click", pushElementsToArray);
 
 
 function pushElementsToArray() {
+  let localItems = JSON.parse(localStorage.getItem("Posts"));
+  if (localItems===null){
+    arrayOfBlog=[];
+  } else {
+    arrayOfBlog = localItems;
+  }
+
   let post = {
     rubric:rubric.value,
     date: date.value,
@@ -39,18 +46,25 @@ function pushElementsToArray() {
 
   }
   arrayOfBlog.push(post);
-   console.log(arrayOfBlog);
-  createElementsOfUser();
-  localStorage.setItem("Posts",JSON.stringify(arrayOfBlog));
   rubric.value = "";
   date.value = "";
   textContent.value = "";
+  localStorage.setItem("Posts",JSON.stringify(arrayOfBlog));
+   DisplayBlogsOfUser();
+
 }
 
-function createElementsOfUser() {
+function DisplayBlogsOfUser() {
+  let localItems = JSON.parse(localStorage.getItem("Posts"));
+  if (localItems===null){
+    arrayOfBlog=[];
+  } else {
+    arrayOfBlog = localItems;
+  }
+
   let output="";
   for (const post of arrayOfBlog) {
-    output = `
+    output += `
     <div class ="container">
       <h1>${post.rubric}</h1>
       <p id="userDate">${post.date}</p>
@@ -59,6 +73,6 @@ function createElementsOfUser() {
 
   `;
 }
-outputs.insertAdjacentHTML("beforebegin",output);
+outputs.innerHTML=output;
 }
-createElementsOfUser();
+DisplayBlogsOfUser();
